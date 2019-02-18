b6bc179e198e2174d2f60ce8acd3f35d95f08c33
Chinmay Chandak <chandakchinmay@gmail.com>
2019-02-18 14:45:56 +0530

    init
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ app.js
@@ -0,0 +1,55 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const expressWs = require('express-ws')(app);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// WebSockets
+app.ws('/echo', (ws, req) => {
+  // console.log(ws);
+  console.log(req.url);
+  ws.on('message', (msg) => {
+    console.log(msg);
+    ws.send();
+  });
+});
+
+// routing
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(8000);
+
+module.exports = app;
