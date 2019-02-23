425f4bf1dcdddf3d26976107f67fe739320f24f1
Chinmay Chandak <chandakchinmay@gmail.com>
2019-02-23 17:28:23 +0530

    little bit of clean-up...
    moved code from app.js to middleware.js
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 // require mandatory modules
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
 // require routings
 const indexRouter = require('./routes/index');
@@ -13,50 +9,23 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // initialize WebSockets routing
-const initWS = require('./controller/wsController');
+const initWS = require('./controllers/wsController');
 initWS();
 
 // // start TURN server
-// const TURN = require('./controller/turnController');
+// const TURN = require('./controllers/turnController');
 // TURN.initTURN();
 
 // init middlewares
 const middleware = require('./config/middleware');
-middleware();
+middleware(app);
 
 // init database
 const database = require('./config/database');
 database();
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 // routing
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
